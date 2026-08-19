bf1795efa782858120283fc0af6114ad1433f74f
Test User <test@example.com>
2026-08-19 17:41:04 +0700

    ThÃªm test case bao phá»§ Ä‘Æ°á»ng Ä‘i. Resolves #2

--- src/test/java/com/demo/AppTest.java
+++ src/test/java/com/demo/AppTest.java
@@ -4,8 +4,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
+    
     @Test
     public void testStatementCoverage() {
+        App app = new App();
+        assertEquals(1, app.countEvenNumbers(2));
+    }
+    
+    @Test
+    public void testPathCoverage_NoLoop() {
+        App app = new App();
+        assertEquals(0, app.countEvenNumbers(0));
+    }
+
+    @Test
+    public void testPathCoverage_LoopOnce_Odd() {
+        App app = new App();
+        assertEquals(0, app.countEvenNumbers(1));
+    }
+
+    @Test
+    public void testPathCoverage_LoopMultiple_Even() {
         App app = new App();
         assertEquals(1, app.countEvenNumbers(2));
     }
