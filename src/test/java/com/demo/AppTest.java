a0779298d0bd90192e70387e91c4ce5fd2c9fada
Test User <test@example.com>
2026-08-19 17:40:58 +0700

    ThÃªm test case bao phá»§ lá»‡nh. Resolves #1

---
+++ src/test/java/com/demo/AppTest.java
@@ -0,0 +1,12 @@
+﻿package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testStatementCoverage() {
+        App app = new App();
+        assertEquals(1, app.countEvenNumbers(2));
+    }
+}
