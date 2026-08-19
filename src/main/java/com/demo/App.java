e627556e253bfa9550cee5fd704ed8929c8fb7b1
Test User <test@example.com>
2026-08-19 17:40:50 +0700

    Khá»Ÿi táº¡o project vá»›i chÆ°Æ¡ng trÃ¬nh Ä‘áº¿m sá»‘ cháºµn (cÃ³ vÃ²ng láº·p vÃ  ráº½ nhÃ¡nh)

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,13 @@
+﻿package com.demo;
+
+public class App {
+    public int countEvenNumbers(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
